2f47b9fa95cf4f7660888c27c637ebdf005f4b96
“Nathan <schroedernathan2011@icloud.com>
2025-01-26 00:48:45 -0500

    Now connects to spotify api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import React from 'react';
 
 export default function Home() {
+
+
   return (
-    <div className="">
-      
+    <div className="home">
+      Hello
     </div>
   );
 }
---
+++ src/pages/api/stats/tracks.js
@@ -0,0 +1,28 @@
+import { topTracks } from "../../../../lib/spotify";
+
+export default async function handler(req, res) {
+  try {
+    const response = await topTracks();
+    if (!response.ok) {
+      throw new Error(`Error fetching top tracks: ${response.statusText}`);
+    }
+    const { items } = await response.json();
+
+    const tracks = items.slice(0, 5).map((track) => ({
+      title: track.name,
+      artist: track.artists.map((_artist) => _artist.name).join(", "),
+      url: track.external_urls.spotify,
+      coverImage: track.album.images[1],
+    }));
+
+    res.setHeader(
+      "Cache-Control",
+      "public, s-maxage=86400, stale-while-revalidate=43200"
+    );
+
+    return res.status(200).json(tracks);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+}
