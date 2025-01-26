32c4a3e5557794283e793744de9b353d09767955
“Nathan <schroedernathan2011@icloud.com>
2025-01-26 14:27:25 -0500

    Now fetches user data

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,12 +2,24 @@
 import React, { useEffect, useState } from "react";
 import Song from "../components/Song";
 import Artist from "../components/Artist";
+import UserHeader from "../components/UserHeader";
 
 const Dashboard = () => {
+  const [user, setUser] = useState<User>();
   const [tracks, setTracks] = useState<Track[]>([]);
   const [topArtists, setTopArtists] = useState<Artist[]>([]);
   useEffect(() => {
     const fetchTracks = async () => {
+      try {
+        const response = await fetch("api/stats/user");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setUser(data);
+      } catch (error) {
+        console.error("Error fetching user:", error);
+      }
       try {
         const response = await fetch("api/stats/tracks");
         if (!response.ok) {
@@ -38,21 +50,24 @@ const Dashboard = () => {
   }, []);
 
   return (
-    <div className="p-4 flex flex-col md:flex-row gap-4 justify-center items-center">
-      <div>
-        <h1 className="text-2xl font-bold">Top Tracks</h1>
-        <div className="flex flex-col gap-4 mt-4">
-          {tracks.map((track, index) => (
-            <Song key={index} track={track} />
-          ))}
+    <div>
+      <UserHeader user={user}/>
+      <div className="p-4 flex flex-wrap gap-4 justify-center items-center">
+        <div>
+          <h1 className="text-2xl font-bold">Top Tracks</h1>
+          <div className="flex flex-col gap-4 mt-4">
+            {tracks.map((track, index) => (
+              <Song key={index} track={track} />
+            ))}
+          </div>
         </div>
-      </div>
-      <div>
-        <h1 className="text-2xl font-bold">Top Artists</h1>
-        <div className="flex flex-col gap-4 mt-4">
-          {topArtists.map((track, index) => (
-            <Artist key={index} artist={track} />
-          ))}
+        <div>
+          <h1 className="text-2xl font-bold">Top Artists</h1>
+          <div className="flex flex-col gap-4 mt-4">
+            {topArtists.map((track, index) => (
+              <Artist key={index} artist={track} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
