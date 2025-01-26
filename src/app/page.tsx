0b8964d065ceedf5bccb0b60390b9dee25ad14c4
“Nathan <schroedernathan2011@icloud.com>
2025-01-26 14:04:52 -0500

    Changed the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,102 @@
-import React from "react";
-
-
 export default function Home() {
   const authUrl = `https://accounts.spotify.com/authorize?client_id=${process.env.CLIENT_ID}&response_type=code&redirect_uri=http://localhost:3000/callback&scope=user-read-currently-playing+user-top-read 
 `;
   return (
-    <div className="home">
-      <a href={authUrl}>Login with Spotify</a>
+    <div className="relative isolate overflow-hidden ">
+      <svg
+        aria-hidden="true"
+        className="absolute inset-0 -z-10 size-full stroke-white/10 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
+      >
+        <defs>
+          <pattern
+            x="50%"
+            y={-1}
+            id="983e3e4c-de6d-4c3f-8d64-b9761d1534cc"
+            width={200}
+            height={200}
+            patternUnits="userSpaceOnUse"
+          >
+            <path d="M.5 200V.5H200" fill="none" />
+          </pattern>
+        </defs>
+        <svg x="50%" y={-1} className="overflow-visible fill-gray-800/20">
+          <path
+            d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
+            strokeWidth={0}
+          />
+        </svg>
+        <rect
+          fill="url(#983e3e4c-de6d-4c3f-8d64-b9761d1534cc)"
+          width="100%"
+          height="100%"
+          strokeWidth={0}
+        />
+      </svg>
+      <div
+        aria-hidden="true"
+        className="absolute top-10 left-[calc(50%-4rem)] -z-10 transform-gpu blur-3xl sm:left-[calc(50%-18rem)] lg:top-[calc(50%-30rem)] lg:left-48 xl:left-[calc(50%-24rem)]"
+      >
+        <div
+          style={{
+            clipPath:
+              "polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)",
+          }}
+          className="aspect-1108/632 w-[69.25rem] bg-linear-to-r from-[#0a3f11] to-[#1bb010] opacity-20"
+        />
+      </div>
+      <div className="mx-auto max-w-7xl px-6 pt-10 pb-24 sm:pb-32 lg:flex lg:px-8 lg:py-40">
+        <div className="mx-auto max-w-2xl shrink-0 lg:mx-0 lg:pt-8">
+          <img
+            alt="Your Company"
+            src="images/spotify-logo.png"
+            className="h-11"
+          />
+          <div className="mt-24 sm:mt-32 lg:mt-16">
+            <a href="#" className="inline-flex space-x-6">
+              <span className="rounded-full bg-green-500/10 px-3 py-1 text-sm/6 font-semibold text-green-400 ring-1 ring-green-500/20 ring-inset">
+                What's new
+              </span>
+              <span className="inline-flex items-center space-x-2 text-sm/6 font-medium text-gray-300">
+                <span>Just shipped v1.0</span>
+                {/* <ChevronRightIcon
+                  aria-hidden="true"
+                  className="size-5 text-gray-500"
+                /> */}
+              </span>
+            </a>
+          </div>
+          <h1 className="mt-10 text-5xl font-semibold tracking-tight text-pretty text-white sm:text-7xl">
+            Spotify AI Analytics
+          </h1>
+          <p className="mt-8 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">
+            Personalized insights about your music taste through AI-powered
+            analytics. Track your listening habits and explore your musical
+            journey with detailed statistics.
+          </p>
+          <div className="mt-10 flex items-center gap-x-6">
+            <a
+              href="#"
+              className="rounded-md bg-green-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-green-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-400"
+            >
+              Get started
+            </a>
+            <a href="#" className="text-sm/6 font-semibold text-white">
+              Learn more <span aria-hidden="true">→</span>
+            </a>
+          </div>
+        </div>
+        <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:mt-0 lg:mr-0 lg:ml-10 lg:max-w-none lg:flex-none xl:ml-32">
+          <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
+            <img
+              alt="App screenshot"
+              src="https://tailwindui.com/plus/img/component-images/dark-project-app-screenshot.png"
+              width={2432}
+              height={1442}
+              className="w-[76rem] rounded-md bg-white/5 ring-1 shadow-2xl ring-white/10"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
